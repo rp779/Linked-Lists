896d65a8287fddf1b481a57869b9cd78e606969b
Ross <49578416+rp779@users.noreply.github.com>
2019-07-08 23:18:33 -0400

    update slist.cpp
    fixed typos in comments

--- slist.cpp
+++ slist.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 
-//The default constructor is used to initialize the state of your class.
-//In this case, the state of your class are the pointers, front and back.
+//The default constructor is used to initialize the state of class.
+//In this case, the state are the pointers, front and back.
 LINKED_LIST_CLASS::LINKED_LIST_CLASS()
 {
 	cout << "Inside the default constructor\n";
@@ -14,47 +14,40 @@ LINKED_LIST_CLASS::LINKED_LIST_CLASS()
 
 }
 
-//The destructor will de-allocate all the memory allocated to your class.
-//It is call automatically by the compiler when the current object goes
-//out of scope.
+//The destructor will de-allocate all the memory allocated by the LIST_NODE class.
 LINKED_LIST_CLASS::~LINKED_LIST_CLASS()
 {
-	cout << "Destructor has been called\n";
 	while (front != 0)
 	{
 		LIST_NODE *p = front;
 		front = front->next;
 		delete p;
 	}
-
 }
 
 //The following function, Print, prints all the data stored in the
 //linked list of the current object. In this case, printing starts at
-//the front of the list.
+//the front of the list. 
 void LINKED_LIST_CLASS::Print()//accessor
 {
 
 	LIST_NODE *p = front;
-
 	cout << endl << endl;
-
 	while (p != 0)
 	{
 		cout << p->data << endl;
 		p = p->next;
 	}
-
 	cout << endl << endl;
 }
 
 //The function, Insert_FRONT, adds a new List_Type data item to the front
-//of the linked list.  If the list is null, memory is allocated for a
-//new node, and key is stored in the data filed, and 0 is stored in the next field,
-// and front and back are set to point to the node.
+//of the linked list.  If the list is NULL, memory is allocated for a
+//new node, and key is stored in the data field, and 0 (NULL) is stored in the next field,
+//and front and back are set to point to the node.
 //
-//If the list is not null, memory is allocated for a new node, 
-//and key is stored in the data filed, and front is stored in the next field,
+//If the list is not NULL, memory is allocated for a new node, 
+//and key is stored in the data field, and front is stored in the next field,
 //and front is set to point to the node.
 void LINKED_LIST_CLASS::Insert_FRONT(const List_Type & key)
 {
@@ -78,16 +71,15 @@ void LINKED_LIST_CLASS::Insert_FRONT(const List_Type & key)
 }
 
 //The function, Insert_BACK, adds a new List_Type data item to the front
-//of the linked list.  If the list is null, memory is allocated for a
-//new node, and key is stored in the data filed, and 0 is stored in the next field,
-// and front and back are set to point to the node.
+//of the linked list.  If the list is NULL, memory is allocated for a
+//new node, and key is stored in the data field, and 0 is stored in the next field,
+//and front and back are set to point to the node.
 //
 //If the list is not null, memory is allocated for a new node, 
 //and key is stored in the data filed, and front is stored in the next field,
 //and front is set to point to the node.
 void LINKED_LIST_CLASS::Insert_BACK(const List_Type & key)
 {
-
 	if (front == 0)
 	{
 		front = new LIST_NODE;
@@ -103,7 +95,6 @@ void LINKED_LIST_CLASS::Insert_BACK(const List_Type & key)
 		back->next = p;
 		back = p;
 	}
-
 }
 
 //The function Create_List creates a linked list from the string s.  A copy of all
@@ -143,8 +134,8 @@ LIST_NODE * LINKED_LIST_CLASS::Search(const List_Type & key)
 
 }
 
-//The function, Remove, removse key from, if it is in the list, 
-//from any locationanywhere (front, back, and between front and back).
+//The function, Remove, removes the node containing the key, if it is in the list, 
+//from any location (front, back, and between front and back).
 void LINKED_LIST_CLASS::Remove(const List_Type & key)
 {
 	//p=0 if key not in list; otherwise p holds the address of the node key is in.
